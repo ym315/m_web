958dee3633fd2253337187e4a2db0afc2ff96ecb
ym315 <ym021315@gamil.com>
2025-07-11 19:29:19 +0900

    upload-first

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    //menu
+    $('header').load('menu.html header>div');
+    $('.main_left').load('menu.html .main_menu, menu.html .main_left_dummy');
+    $('.main_left_dummy').load('menu.html .main_left_dummy>div');
+    // $('footer').load('menu.html footer>ul');
+    // $('aside').load('menu.html aside');   
+
+
+    $(document).on('click', '.menu-btn', function() {
+        $('.menu-btn').toggleClass('open');
+        $('.mobile-menu').toggleClass('open');
+        $('.overlay').toggleClass('open');
+        $('body').toggleClass('menu-open');
+    });
+    $(document).on('click', '.overlay', function () {
+    $('.menu-btn').removeClass('open');
+    $('.mobile-menu').removeClass('open');
+    $('.overlay').removeClass('open');
+    $('body').removeClass('menu-open');
+    });
+
+    
+});
+
