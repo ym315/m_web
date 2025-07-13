dd191440d81c5788916f8fd06bee099cdd0b4d84
ym315 <ym021315@gmail.com>
2025-07-13 14:33:22 +0900

    아이폰기준 수정
    메뉴 스크롤, 차트, 날짜 인풋 가로, 팝업가로

--- js/menu.js
+++ js/menu.js
@@ -21,7 +21,13 @@ $(document).ready(function(){
         $('body').removeClass('menu-open');
     });
 
-   
+    function setVh() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setVh();
+    window.addEventListener('resize', setVh);
+    
 
 });
 
